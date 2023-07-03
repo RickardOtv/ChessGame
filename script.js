8b9c0c84c3fbc9a0c68065049b8ab21e227ba505
Rickard Otvos <rickard.0413@gmail.com>
2023-07-03 14:23:17 +0200

    addes some stuff

---
+++ script.js
@@ -0,0 +1,301 @@
+const pieces = [
+  "r",
+  "n",
+  "b",
+  "q",
+  "k",
+  "b",
+  "n",
+  "r",
+  "p",
+  "p",
+  "p",
+  "p",
+  "p",
+  "p",
+  "p",
+  "p",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "P",
+  "P",
+  "P",
+  "P",
+  "P",
+  "P",
+  "P",
+  "P",
+  "R",
+  "N",
+  "B",
+  "Q",
+  "K",
+  "B",
+  "N",
+  "R",
+];
+
+let selectedPiece = null;
+let selectedCell = null;
+let isWhiteTurn = true;
+const capturedPieces = {
+  white: [],
+  black: [],
+};
+
+function createBoard() {
+  const board = document.getElementById("board");
+  for (let i = 0; i < 64; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+
+    // Add class for square color
+    if ((i + Math.floor(i / 8)) % 2 === 0) {
+      cell.classList.add("square-light");
+    } else {
+      cell.classList.add("square-dark");
+    }
+
+    cell.addEventListener("click", handleCellClick);
+    board.appendChild(cell);
+  }
+}
+
+function updateBoard() {
+  const cells = document.getElementsByClassName("cell");
+  for (let i = 0; i < 64; i++) {
+    const cell = cells[i];
+    cell.textContent = "";
+    cell.classList.remove("selected");
+    const piece = pieces[i];
+    if (piece !== "") {
+      const pieceElement = document.createElement("span");
+      pieceElement.textContent = piece;
+      pieceElement.classList.add("piece");
+      if (piece === piece.toUpperCase()) {
+        pieceElement.classList.add("white-piece");
+      } else {
+        pieceElement.classList.add("black-piece");
+      }
+      cell.appendChild(pieceElement);
+    }
+  }
+}
+
+function handleCellClick() {
+  const index = Array.from(this.parentNode.children).indexOf(this);
+  const piece = pieces[index];
+  if (selectedPiece === null) {
+    if (
+      piece !== "" &&
+      (isWhiteTurn
+        ? piece === piece.toUpperCase()
+        : piece === piece.toLowerCase())
+    ) {
+      selectedPiece = piece;
+      selectedCell = this;
+      this.classList.add("selected");
+    }
+  } else {
+    if (selectedCell === this) {
+      // Deselect the piece
+      selectedPiece = null;
+      selectedCell.classList.remove("selected");
+    } else {
+      const isValidMove = validateMove(selectedCell, this);
+      if (isValidMove) {
+        const sourceIndex = Array.from(
+          selectedCell.parentNode.children
+        ).indexOf(selectedCell);
+        const targetIndex = index;
+        pieces[targetIndex] = selectedPiece;
+        pieces[sourceIndex] = "";
+        isWhiteTurn = !isWhiteTurn;
+        selectedPiece = null;
+        selectedCell.classList.remove("selected");
+        updateBoard();
+        updateCapturedPieces();
+        updateStatus();
+      }
+    }
+  }
+}
+
+function validateMove(sourceCell, targetCell) {
+  const sourceIndex = Array.from(sourceCell.parentNode.children).indexOf(
+    sourceCell
+  );
+  const targetIndex = Array.from(targetCell.parentNode.children).indexOf(
+    targetCell
+  );
+  const piece = pieces[sourceIndex];
+  const targetPiece = pieces[targetIndex];
+  const pieceType = piece.toLowerCase();
+
+  const sourceRow = Math.floor(sourceIndex / 8);
+  const sourceCol = sourceIndex % 8;
+  const targetRow = Math.floor(targetIndex / 8);
+  const targetCol = targetIndex % 8;
+
+  if (sourceIndex === targetIndex) {
+    return false;
+  }
+
+  if (pieceType === "p") {
+    const direction = isWhiteTurn ? -1 : 1;
+    if (targetCol === sourceCol && targetPiece === "") {
+      if (targetRow === sourceRow + direction) {
+        return true;
+      } else if (
+        sourceRow === 1 &&
+        targetRow === 3 &&
+        pieces[sourceIndex + 8] === ""
+      ) {
+        return true;
+      }
+    } else if (
+      (targetCol === sourceCol - 1 || targetCol === sourceCol + 1) &&
+      targetRow === sourceRow + direction &&
+      targetPiece !== "" &&
+      (isWhiteTurn
+        ? targetPiece === targetPiece.toLowerCase()
+        : targetPiece === targetPiece.toUpperCase())
+    ) {
+      return true;
+    }
+  } else if (pieceType === "r") {
+    if (sourceRow === targetRow || sourceCol === targetCol) {
+      if (isPathClear(sourceRow, sourceCol, targetRow, targetCol)) {
+        return true;
+      }
+    }
+  } else if (pieceType === "n") {
+    const rowDiff = Math.abs(targetRow - sourceRow);
+    const colDiff = Math.abs(targetCol - sourceCol);
+    return (rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2);
+  } else if (pieceType === "b") {
+    const rowDiff = Math.abs(targetRow - sourceRow);
+    const colDiff = Math.abs(targetCol - sourceCol);
+    if (rowDiff === colDiff) {
+      if (isPathClear(sourceRow, sourceCol, targetRow, targetCol)) {
+        return true;
+      }
+    }
+  } else if (pieceType === "q") {
+    if (sourceRow === targetRow || sourceCol === targetCol) {
+      if (isPathClear(sourceRow, sourceCol, targetRow, targetCol)) {
+        return true;
+      }
+    } else {
+      const rowDiff = Math.abs(targetRow - sourceRow);
+      const colDiff = Math.abs(targetCol - sourceCol);
+      if (rowDiff === colDiff) {
+        if (isPathClear(sourceRow, sourceCol, targetRow, targetCol)) {
+          return true;
+        }
+      }
+    }
+  } else if (pieceType === "k") {
+    const rowDiff = Math.abs(targetRow - sourceRow);
+    const colDiff = Math.abs(targetCol - sourceCol);
+    return (
+      (rowDiff === 0 && colDiff === 1) ||
+      (colDiff === 0 && rowDiff === 1) ||
+      (rowDiff === 1 && colDiff === 1)
+    );
+  }
+
+  return false;
+}
+
+function isPathClear(sourceRow, sourceCol, targetRow, targetCol) {
+  const rowStep = targetRow > sourceRow ? 1 : targetRow < sourceRow ? -1 : 0;
+  const colStep = targetCol > sourceCol ? 1 : targetCol < sourceCol ? -1 : 0;
+  let row = sourceRow + rowStep;
+  let col = sourceCol + colStep;
+
+  while (row !== targetRow || col !== targetCol) {
+    const index = row * 8 + col;
+    if (pieces[index] !== "") {
+      return false;
+    }
+    row += rowStep;
+    col += colStep;
+  }
+
+  return true;
+}
+
+function updateCapturedPieces() {
+  const capturedPiecesContainer = document.getElementById("captured-pieces");
+  capturedPiecesContainer.innerHTML = "";
+
+  for (const color in capturedPieces) {
+    for (const piece of capturedPieces[color]) {
+      const pieceElement = document.createElement("span");
+      pieceElement.textContent = piece;
+      pieceElement.classList.add("captured-piece");
+      if (color === "white") {
+        pieceElement.classList.add("white-piece");
+      } else {
+        pieceElement.classList.add("black-piece");
+      }
+      capturedPiecesContainer.appendChild(pieceElement);
+    }
+  }
+}
+
+function updateStatus() {
+  const status = document.getElementById("status");
+  if (isCheckmate()) {
+    status.textContent = isWhiteTurn ? "Black wins!" : "White wins!";
+  } else if (isStalemate()) {
+    status.textContent = "Stalemate!";
+  } else {
+    status.textContent = isWhiteTurn ? "White's turn" : "Black's turn";
+  }
+}
+
+function isCheckmate() {
+  // TODO: Implement checkmate logic
+  return false;
+}
+
+function isStalemate() {
+  // TODO: Implement stalemate logic
+  return false;
+}
+
+createBoard();
+updateBoard();
+updateStatus();
