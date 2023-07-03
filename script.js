8bafeff9d55bee9076ebdd6133638b7a55a0132c
Rickard Otvos <rickard.0413@gmail.com>
2023-07-03 15:16:46 +0200

    added highlights, missig checkmate

--- script.js
+++ script.js
@@ -141,6 +141,7 @@ function handleCellClick() {
     selectedPiece = null;
     selectedCell.classList.remove("selected");
     selectedCell = null;
+    removeValidMoveHighlights(); // Remove the valid move highlights
     return;
   }
 
@@ -154,6 +155,7 @@ function handleCellClick() {
       selectedPiece = piece;
       selectedCell = this;
       this.classList.add("selected");
+      highlightValidMoves(selectedCell); // Highlight the valid moves for the selected piece
     }
   } else {
     const isValidMove = validateMove(selectedCell, this);
@@ -172,6 +174,7 @@ function handleCellClick() {
       ) {
         selectedPiece = null;
         selectedCell.classList.remove("selected");
+        removeValidMoveHighlights(); // Remove the valid move highlights
         return;
       }
 
@@ -180,10 +183,56 @@ function handleCellClick() {
       isWhiteTurn = !isWhiteTurn;
       selectedPiece = null;
       selectedCell.classList.remove("selected");
+      removeValidMoveHighlights(); // Remove the valid move highlights
       updateBoard();
       updateCapturedPieces();
       updateStatus();
+      removeCaptureColorHighlights(); // Remove the capture color highlights
     }
+  }
+}
+
+function removeCaptureColorHighlights() {
+  const captureMoves = document.getElementsByClassName("capture-move");
+  while (captureMoves.length > 0) {
+    captureMoves[0].classList.remove("capture-move");
+  }
+}
+
+function highlightValidMoves(sourceCell) {
+  const sourceIndex = Array.from(sourceCell.parentNode.children).indexOf(
+    sourceCell
+  );
+  const piece = pieces[sourceIndex];
+
+  const cells = document.getElementsByClassName("cell");
+  for (let i = 0; i < cells.length; i++) {
+    const cell = cells[i];
+    const targetIndex = Array.from(cell.parentNode.children).indexOf(cell);
+    const targetPiece = pieces[targetIndex];
+
+    // Skip empty cells and cells with friendly pieces
+    if (
+      targetPiece !== "" &&
+      ((isWhiteTurn && targetPiece === targetPiece.toUpperCase()) ||
+        (!isWhiteTurn && targetPiece === targetPiece.toLowerCase()))
+    ) {
+      continue;
+    }
+
+    if (validateMove(sourceCell, cell)) {
+      // Check if the move captures a piece
+      const isCaptureMove = targetPiece !== "";
+
+      cell.classList.add(isCaptureMove ? "capture-move" : "valid-move");
+    }
+  }
+}
+
+function removeValidMoveHighlights() {
+  const validMoves = document.getElementsByClassName("valid-move");
+  while (validMoves.length > 0) {
+    validMoves[0].classList.remove("valid-move");
   }
 }
 
@@ -324,13 +373,104 @@ function updateStatus() {
 }
 
 function isCheckmate() {
-  // TODO: Implement checkmate logic
+  const kingColor = isWhiteTurn ? "K" : "k";
+  const kingIndex = pieces.indexOf(kingColor);
+
+  // Check if the king is in check
+  if (isKingUnderAttack(kingIndex)) {
+    // Iterate over all pieces of the same color as the king
+    for (let i = 0; i < pieces.length; i++) {
+      const piece = pieces[i];
+
+      if (piece.toLowerCase() === kingColor.toLowerCase()) {
+        // Check if any of the king's legal moves can get it out of check
+        const legalMoves = getLegalMoves(i);
+
+        for (let j = 0; j < legalMoves.length; j++) {
+          const move = legalMoves[j];
+
+          // Simulate the move and check if the king is still in check
+          const tempPiece = pieces[move.to];
+          pieces[move.to] = piece;
+          pieces[move.from] = "";
+          const isStillInCheck = isKingUnderAttack(kingIndex);
+
+          // Undo the move
+          pieces[move.from] = piece;
+          pieces[move.to] = tempPiece;
+
+          // If the king is not in check, it's not checkmate
+          if (!isStillInCheck) {
+            return false;
+          }
+        }
+      }
+    }
+
+    // If no legal moves can get the king out of check, it's checkmate
+    return true;
+  }
+
+  // If the king is not in check, it's not checkmate
   return false;
 }
 
 function isStalemate() {
-  // TODO: Implement stalemate logic
+  // Check if the current player has any legal moves
+  for (let i = 0; i < 64; i++) {
+    const piece = pieces[i];
+    if (
+      piece !== "" &&
+      ((isWhiteTurn && piece === piece.toUpperCase()) ||
+        (!isWhiteTurn && piece === piece.toLowerCase()))
+    ) {
+      for (let j = 0; j < 64; j++) {
+        if (validateMove(getCellByIndex(i), getCellByIndex(j))) {
+          return false; // The player has a legal move, so it's not stalemate
+        }
+      }
+    }
+  }
+
+  // The current player has no legal moves, so it's stalemate
+  return true;
+}
+
+function isKingUnderAttack(kingIndex) {
+  const kingColor = pieces[kingIndex].toLowerCase();
+  const opponentColor = kingColor === "k" ? "b" : "w";
+
+  // Check if any opponent's piece can capture the king
+  for (let i = 0; i < 64; i++) {
+    const piece = pieces[i];
+    if (
+      piece !== "" &&
+      (piece === piece.toUpperCase()
+        ? piece.toLowerCase() === kingColor
+        : piece && piece.toUpperCase() === kingColor)
+    ) {
+      continue; // Skip friendly pieces
+    }
+
+    if (
+      piece !== "" &&
+      (piece === piece.toUpperCase()
+        ? piece.toLowerCase() === opponentColor
+        : piece && piece.toUpperCase() === opponentColor)
+    ) {
+      if (validateMove(getCellByIndex(i), getCellByIndex(kingIndex))) {
+        return true; // The king is under attack
+      }
+    }
+  }
+
+  // The king is not under attack
   return false;
+}
+
+function getCellByIndex(index) {
+  const cells = document.getElementsByClassName("cell");
+  return cells[index];
 }
 
 createBoard();
